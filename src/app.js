09906bc6a317b5ecd1c2d547e342556a88cdaa90
ahmedtahatekin <tekinahmedtaha456@gmail.com>
2026-03-28 13:10:30 +0300

    rotalar tanımlandı

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import indexRouter from "./routes/index.route.js";
 
 dotenv.config();
 
@@ -9,11 +10,13 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.set("view engine", "ejs");  
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
