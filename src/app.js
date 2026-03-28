9ffc63315167691ca30168bcf6e6f1c1801eaae5
ahmedtahatekin <tekinahmedtaha456@gmail.com>
2026-03-28 10:00:36 +0300

    todo eklendi

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
