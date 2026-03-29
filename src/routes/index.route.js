afc5e441e50c583d0b13f4bc41439d363eec05a7
zekigoregen440-tech <zekigoregen440@gmail.com>
2026-03-29 15:47:41 +0300

    heleleelelelelel

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -50,40 +50,8 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/gsfgsgfas', async (req, res) => {
-    try {
-        // Tüm istekleri paralel başlat
-        const [nasaRes, noaaAlertRes, kpRes, scalesRes] = await Promise.all([
-            fetch(`https://api.nasa.gov/DONKI/FLR?startDate=2026-03-20&endDate=2026-03-28&api_key=Bvi0EPVQqCsdyuoWcGh9rFK20n0qFrPFP6UJ04Al`),
-            fetch("https://services.swpc.noaa.gov/products/alerts.json"),
-            fetch("https://services.swpc.noaa.gov/json/planetary_k_index_1m.json"),
-            fetch("https://services.swpc.noaa.gov/products/noaa-scales.json")
-        ]);
-
-        // Verileri JSON'a çevir (AWAIT ŞART!)
-        const nasaData = await nasaRes.json();
-        const noaaAlerts = await noaaAlertRes.json();
-        const kpData = await kpRes.json();
-        const noaaScales = await scalesRes.json();
-
-        // LOG AT VE KONTROL ET (Burada veri var mı?)
-        // console.log("RAW NASA:", nasaData); 
-
-        // ŞİMDİ BİRLEŞTİR
-        const finalData = mergeSpaceWeatherData(nasaData, noaaAlerts, kpData, noaaScales);
-
-        res.json(finalData);
-   } catch (error) {
-        // Hatanın ne olduğunu terminale kırmızı kırmızı yazdır
-        console.error("GERÇEK HATA BURADA:", error);
-        
-        // Ekrana da hatanın detayını yolla ki görelim
-        res.status(500).json({ 
-            mesaj: "Veriler alınamadı", 
-            hata_detayi: error.message,
-            hata_turu: error.name
-        });
-    }
+router.get("/gunes-firtinalari-nedir", (request, response) => {
+    response.render("info");
 });
 
 export default router;
